c2a6f059d2c1d66cf4ab22e74038c022040795da
GHDsw <hsw29371@gmail.com>
2025-09-16 10:50:56 +0900

    사각 운동, 원 운동 함수 뼈대 완성

--- character_move.py
+++ character_move.py
@@ -3,5 +3,17 @@ from pico2d import *
 open_canvas()
 
 # fill here
+def moveRect():
+    pass
+
+
+def moveCircle():
+    pass
+
+
+while True:
+    moveRect()
+    moveCircle()
+    pass
 
 close_canvas()

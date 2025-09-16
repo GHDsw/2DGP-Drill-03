240b2f92b5f3d8bf4fe749a95ad7ba8315066801
GHDsw <hsw29371@gmail.com>
2025-09-16 11:02:36 +0900

    캐릭터 출력 완료

--- character_move.py
+++ character_move.py
@@ -2,6 +2,8 @@ from pico2d import *
 
 open_canvas()
 
+boy = load_image('character.png')
+
 # fill here
 def moveRect():
     print ("moveRect")
@@ -10,12 +12,16 @@ def moveRect():
 
 def moveCircle():
     print ("moveCircle")
+    clear_canvas_now()
+    boy.draw_now(400,300)
+    delay(0.1)
     pass
 
 
 while True:
-    moveRect()
     moveCircle()
+    moveRect()
+    break
     pass
 
 close_canvas()

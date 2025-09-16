e0c845cecf8bb939add92cca45ec84d639305aef
GHDsw <hsw29371@gmail.com>
2025-09-16 11:56:39 +0900

    moveRect 함수 뼈대 제작 및 작동 확인

--- character_moves.py
+++ character_moves.py
@@ -2,10 +2,35 @@ from pico2d import *
 
 open_canvas()
 
+def moveTop():
+    print('Moving top')
+    pass
+
+
+def moveRight():
+    print('Moving right')
+    pass
+
+
+def moveBottom():
+    print('Moving bottom')
+    pass
+
+
+def moveLeft():
+    print('Moving left')
+    pass
+
 def moveRect():
+    print('Move Rect')
+    moveTop()
+    moveRight()
+    moveBottom()
+    moveLeft()
     pass
 
 def moveCircle():
+    print('Move Circle')
     pass
 
 while True:

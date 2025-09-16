69a871b423b5f6cbaa0e452c8cfa45dfbd784bef
GHDsw <hsw29371@gmail.com>
2025-09-16 11:54:28 +0900

    move 기본 뼈대 제작

---
+++ character_moves.py
@@ -0,0 +1,17 @@
+from pico2d import *
+
+open_canvas()
+
+def moveRect():
+    pass
+
+def moveCircle():
+    pass
+
+while True:
+    moveCircle()
+    moveRect()
+    break
+    pass
+
+close_canvas()
